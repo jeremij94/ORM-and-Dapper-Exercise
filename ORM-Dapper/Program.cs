adde43d7b56d2508db909f8c28b18877e6da0639
jeremij94 <jeremij94@gmail.com>
2022-10-26 09:46:29 -0500

    ORM and Dapper Exercise git ignore add

--- ORM-Dapper/Program.cs
+++ ORM-Dapper/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace ORM_Dapper
+﻿using Dapper;
+using MySql.Data.MySqlClient;
+using System.Data;
+namespace ORM_Dapper
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
+
+            var connectionString = "Server=localhost;Database=bestbuy;Uid-root;Pwd=password;";
+
+            var conn = new MySqlConnection(connectionString);
+
+            IEnumerable<Department> GetDepartments()
+            {
+                return conn.Query<Department>("SELECT * from departments;");
+            }
         }
     }
 }
